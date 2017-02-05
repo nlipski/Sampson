4644d3f83a2d2cc2fffb951fdf73b4d04c1600fb
Jonathon Malcolm <j_malcolm@outlook.com>
2017-02-04 21:20:25 -0500

    Updated Arduino

--- qhacksV2/qhacksV2.ino
+++ qhacksV2/qhacksV2.ino
@@ -1,25 +1,10 @@
-/*
-  Hello World.ino
-  2013 Copyright (c) Seeed Technology Inc.  All right reserved.
+/* Sampson - The Pill dispenser of the future
 
-  Author:Loovee
-  2013-9-18
+Jonathon Malcolm
+Nikita Lipski
+Connor Sparling
+Jordan O'Neill
 
-  Grove - Serial LCD RGB Backlight demo.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #include <Wire.h>
@@ -27,23 +12,19 @@
 #include <Servo.h>
 
 Servo drug1;
-
 rgb_lcd lcd;
 
-const int colorR = 125;
-const int colorG = 125;
-const int colorB = 125;
 const int drug1Sens = 0;
 const int drug1Servo = 9;
 const int drop1Thresh = 70;
 const int SERVOTIME = 10;
-int open = 105; // open angle
-int close = 30; // close angle
+const int open = 105; // open angle
+const int close = 30; // close angle
 
 void setup() {
   Serial.begin(115200);
   drug1.attach(drug1Servo);
-  drug1.write(close);
+  toClose();
   lcd.begin(16, 2);
   lcd.setColor(0);
   lcd.setCursor(3,0);
@@ -54,13 +35,14 @@ void setup() {
 }
 
 void loop() {
+  displayTime();
   int test = dispense1(5);
   displayDispense(test);
   toClose();
   //displayInstruct("Please take with water");
-  lcd.clear();
   lcd.setColor(0);
   delay(5000);
+  lcd.clear();
   
 }
 
@@ -96,8 +78,6 @@ int dispense1(int amount){ // returns -1 if dispensed correctly
       }
       drug1.write(ang);
       count++;
-      //Serial.println(count);
-
     }
     if (trigger == 0){
       return(amount + 1 - i);
@@ -171,17 +151,11 @@ void displayInstruct(String text){
   lcd.clear();
 }
 
-
-
-void displayTime(){
-  int curHour = hour();
-  Serial.print(curHour);
-  Serial.print("  ");
-  int curDay = day();
-  Serial.println(curDay);
-}
-
 */
+
+
+
+
 /*********************************************************************************************************
   END FILE
 *********************************************************************************************************/
